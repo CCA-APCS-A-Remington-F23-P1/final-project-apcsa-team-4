13ee07dc00c61f44f85efe60980b12f70a277fb8
jhajhriay8809 <jhajhriay8809@my.sduhsd.net>
2023-12-04 09:57:38 -0800

    Note constructors, private fields, getters/setters

--- Note.java
+++ Note.java
@@ -1,3 +1,28 @@
 public class Note {
     
+    private final String PITCH;
+    private final String ACCIDENTAL;
+    private final int OCTAVE;
+
+    public Note(String PITCH, String ACCIDENTAL) {
+        this(PITCH, ACCIDENTAL, 3);
+    }
+
+    public Note(String PITCH, String ACCIDENTAL, int OCTAVE) {
+        this.PITCH = PITCH;
+        this.ACCIDENTAL = ACCIDENTAL;
+        this.OCTAVE = OCTAVE;
+    }
+
+    public String getPitch() {
+        return PITCH;
+    }
+
+    public String getAccidental() {
+        return ACCIDENTAL;
+    }
+
+    public int getOctave() {
+        return OCTAVE;
+    }
 }

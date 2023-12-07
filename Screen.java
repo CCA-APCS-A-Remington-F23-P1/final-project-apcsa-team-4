2620e56f60421a19e15aa402fa76e22ec2925bed
agheyi <anwikagheyi@gmail.com>
2023-12-07 09:06:26 -0800

    Raw Screen.java

---
+++ Screen.java
@@ -0,0 +1,156 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Canvas;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import static java.lang.Character.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.time.*;
+import java.awt.Font;
+
+public class Screen extends Canvas implements KeyListener, Runnable
+{
+  private boolean[] keys;
+  private BufferedImage back;
+
+  public Screen()
+  {
+    setBackground(Color.white);
+
+    keys = new boolean[6];
+
+    this.addKeyListener(this);
+    new Thread(this).start();
+
+    setVisible(true);
+  }
+
+  public void update(Graphics window)
+  {
+    paint(window);
+  }
+
+  public void paint( Graphics window )
+  {
+    //set up the double buffering to make the game animation nice and smooth
+    Graphics2D twoDGraph = (Graphics2D)window;
+
+    //take a snap shop of the current screen and same it as an image
+    //that is the exact same width and height as the current screen
+    if (back==null)
+      back = (BufferedImage)(createImage(getWidth(),getHeight()));
+
+    //create a graphics reference to the back ground image
+    //we will draw all changes on the background image
+    Graphics graphToBack = back.createGraphics();
+
+    
+    graphToBack.setColor(Color.white);
+    graphToBack.fillRect(0,0,800,600);
+    
+
+    if (keys[0])
+    {
+    }
+    else if (keys[1])
+    {
+    
+    }
+    else if (keys[2])
+    {
+        
+    }
+    else if (keys[3])
+    {
+    
+    }
+    else if (keys[4]) {
+    }
+    
+
+      twoDGraph.drawImage(back, null, 0, 0);
+  }
+
+
+  public void keyPressed(KeyEvent e)
+  {
+    if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    {
+      keys[0] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    {
+      keys[1] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_UP)
+    {
+      keys[2] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_DOWN)
+    {
+      keys[3] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_SPACE)
+    {
+      keys[4] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_R) {
+      keys[5] = true;
+    }
+    repaint();
+  }
+
+  public void keyReleased(KeyEvent e)
+  {
+    if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    {
+      keys[0] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    {
+      keys[1] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_UP)
+    {
+      keys[2] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_DOWN)
+    {
+      keys[3] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_SPACE)
+    {
+      keys[4] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_R)
+    {
+      keys[5] = false;
+    }
+    
+    repaint();
+  }
+
+  public void keyTyped(KeyEvent e)
+  {
+  }
+
+  public void run()
+  {
+    try
+    {
+      while(true)
+      {
+        Thread.currentThread().sleep(5);
+        repaint();
+      }
+    }catch(Exception e)
+    {
+    }
+  }
+}
+
+

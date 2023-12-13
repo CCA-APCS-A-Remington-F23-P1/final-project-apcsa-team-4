4c340a7004f2baefacdbc7a7b7720694d4cf5064
agheyi <anwikagheyi@gmail.com>
2023-12-13 11:02:37 -0800

    song uses notes

--- Song.java
+++ Song.java
@@ -1,7 +1,34 @@
+import java.util.*;
+
 public class Song {
-    private Note[] song;
+    private ArrayList<Note> song;
+    private static String[] songs = {"twinkle twinkle", "row your boat", "itsy bitsy spider"};
 
     public Song() {
+        setSong("twinkle twinkle");
+    }
 
+    public Song(String s) {
+        setSong(s);
+    }
+
+    public void setSong(String s) {
+        if (s.equals("twinkle twinkle")) {
+            song.add(new Note('c', 'n', 4));
+            song.add(new Note('c', 'n', 4));
+            song.add(new Note('g', 'n', 4));
+            song.add(new Note('g', 'n', 4));
+            song.add(new Note('a', 'n', 4));
+            song.add(new Note('a', 'n', 4));
+            song.add(new Note('g', 'n', 4));
+            song.add(new Note('f', 'n', 4));
+            song.add(new Note('f', 'n', 4));
+            song.add(new Note('e', 'n', 4));
+            song.add(new Note('e', 'n', 4));
+            song.add(new Note('d', 'n', 4));
+            song.add(new Note('d', 'n', 4));
+            song.add(new Note('c', 'n', 4));
+        }
+        
     }
 }
